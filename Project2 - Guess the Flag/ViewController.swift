fef4615cb9e717d73dacbac177c6c66be66dbca4
John Kim <cloudiosx@gmail.com>
2022-01-20 19:33:06 -0500

    first commit

---
+++ Project2 - Guess the Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2 - Guess the Flag
+//
+//  Created by John Kim on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
